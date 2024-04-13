8eef4adf030b3ec9ab3d580847d20a1eaa9752d0
Sadie Tayler <sadietayler@gmail.com>
2024-04-13 05:00:05 -0600

    add quote thing

--- public/history.js
+++ public/history.js
@@ -28,6 +28,26 @@ function loadScores() {
     } else {
         tableBodyEl.innerHTML = '<tr><td colspan="4">Be the first to play</td></tr>';
     }
-  }
+}
+
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote');
   
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+}
+
+displayQuote();
 document.addEventListener('DOMContentLoaded', loadScores);
